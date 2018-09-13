07e6cce2a1c892019761361b8979929041964c0f
soukypro <soukypro@gmail.com>
2018-09-13 15:28:03 +0200

    Edits
    edits

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+ <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link" :class="activeClass('home')">Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/user" class="nav-link" :class="activeClass('user')">User</router-link>
+                </li>
+                
+            </ul>
+</template>
+<script>
+export default {
+  data(){
+    return {
+
+    };
+  },
+   methods: {
+	  activeClass(...names) {
+        for (let name of names) {
+    		if (name === this.$route.name)
+    			return 'active';
+        }
+    }    
+  }
+}
+</script>
+
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import User from "./components/user/User.vue";
 import Home from "./components/Home.vue";
 export const routes = [
-  { path: "", component: Home },
-  { path: "/user", component: User }
+  { path: "", name: "home", component: Home },
+  { path: "/user", name: "user", component: User }
 ];
